9307d5362e73289b90b5efb236255d6022a93674
Stepan <st.kulagin7@gmail.com>
2023-02-17 20:36:53 +0300

    ChangeDeposit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100;
-        int deposit = 2000;
+        int deposit = 1050;
 
         int bonus;
-        if (deposit > 1100) {
+        if (deposit > 1000) {
             bonus = deposit / 100;
         } else {
             bonus = 0;
